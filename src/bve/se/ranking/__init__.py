ee4d719c782755286714bfe2cdb4845725a0c959
Doug <djmanning18@gmail.com>
2026-07-11 15:28:37 -0400

    feat(se): export acquisition ranking boundary

--- src/bve/se/ranking/__init__.py
+++ src/bve/se/ranking/__init__.py
@@ -4,12 +4,24 @@ from bve.se.ranking.buyer import (
     build_buyer_advantage_hypothesis,
     build_screening_route_hypothesis,
 )
+from bve.se.ranking.acquisition import (
+    AcquisitionCandidate,
+    AcquisitionRankingResult,
+    DiligenceItem,
+    RankedAcquisition,
+    rank_acquisition_candidates,
+)
 from bve.se.ranking.engine import rank_profiles
 from bve.se.ranking.pairwise import compare_profiles
 
 __all__ = [
     "build_buyer_advantage_hypothesis",
     "build_screening_route_hypothesis",
+    "AcquisitionCandidate",
+    "AcquisitionRankingResult",
     "compare_profiles",
+    "DiligenceItem",
     "rank_profiles",
+    "rank_acquisition_candidates",
+    "RankedAcquisition",
 ]
